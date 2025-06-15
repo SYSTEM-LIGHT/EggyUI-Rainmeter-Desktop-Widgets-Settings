a3363ad5d0b25b0fe6d64ca73e7a83b7eb37db75
Alex Smith <klikehep@outlook.com>
2025-06-15 17:01:32 +0800

    添加项目文件。

---
+++ Settings_Window.Designer.vb
@@ -0,0 +1,320 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Settings_Window
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Settings_Window))
+        Panel1 = New Panel()
+        header6 = New PictureBox()
+        header5 = New PictureBox()
+        header4 = New PictureBox()
+        customheader = New PictureBox()
+        header8 = New PictureBox()
+        header7 = New PictureBox()
+        header3 = New PictureBox()
+        header2 = New PictureBox()
+        header1 = New PictureBox()
+        selectcustomheader = New OpenFileDialog()
+        mainheader = New PictureBox()
+        Label1 = New Label()
+        Button1 = New Button()
+        Label2 = New Label()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        ContextMenuStrip1 = New ContextMenuStrip(components)
+        ToolStripMenuItem1 = New ToolStripMenuItem()
+        Panel1.SuspendLayout()
+        CType(header6, ComponentModel.ISupportInitialize).BeginInit()
+        CType(header5, ComponentModel.ISupportInitialize).BeginInit()
+        CType(header4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(customheader, ComponentModel.ISupportInitialize).BeginInit()
+        CType(header8, ComponentModel.ISupportInitialize).BeginInit()
+        CType(header7, ComponentModel.ISupportInitialize).BeginInit()
+        CType(header3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(header2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(header1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(mainheader, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        ContextMenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        Panel1.BackColor = Color.FromArgb(CByte(15), CByte(108), CByte(230))
+        Panel1.Controls.Add(header6)
+        Panel1.Controls.Add(header5)
+        Panel1.Controls.Add(header4)
+        Panel1.Controls.Add(customheader)
+        Panel1.Controls.Add(header8)
+        Panel1.Controls.Add(header7)
+        Panel1.Controls.Add(header3)
+        Panel1.Controls.Add(header2)
+        Panel1.Controls.Add(header1)
+        Panel1.Location = New Point(10, 220)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(400, 400)
+        Panel1.TabIndex = 0
+        ' 
+        ' header6
+        ' 
+        header6.BackgroundImageLayout = ImageLayout.None
+        header6.Image = CType(resources.GetObject("header6.Image"), Image)
+        header6.Location = New Point(290, 160)
+        header6.Name = "header6"
+        header6.Size = New Size(80, 80)
+        header6.SizeMode = PictureBoxSizeMode.Zoom
+        header6.TabIndex = 8
+        header6.TabStop = False
+        ' 
+        ' header5
+        ' 
+        header5.BackgroundImageLayout = ImageLayout.None
+        header5.Image = CType(resources.GetObject("header5.Image"), Image)
+        header5.Location = New Point(160, 160)
+        header5.Name = "header5"
+        header5.Size = New Size(80, 80)
+        header5.SizeMode = PictureBoxSizeMode.Zoom
+        header5.TabIndex = 7
+        header5.TabStop = False
+        ' 
+        ' header4
+        ' 
+        header4.BackgroundImageLayout = ImageLayout.None
+        header4.Image = CType(resources.GetObject("header4.Image"), Image)
+        header4.Location = New Point(30, 160)
+        header4.Name = "header4"
+        header4.Size = New Size(80, 80)
+        header4.SizeMode = PictureBoxSizeMode.Zoom
+        header4.TabIndex = 6
+        header4.TabStop = False
+        ' 
+        ' customheader
+        ' 
+        customheader.BackgroundImageLayout = ImageLayout.None
+        customheader.Image = CType(resources.GetObject("customheader.Image"), Image)
+        customheader.Location = New Point(290, 290)
+        customheader.Name = "customheader"
+        customheader.Size = New Size(80, 80)
+        customheader.SizeMode = PictureBoxSizeMode.Zoom
+        customheader.TabIndex = 5
+        customheader.TabStop = False
+        ' 
+        ' header8
+        ' 
+        header8.BackgroundImageLayout = ImageLayout.None
+        header8.Image = CType(resources.GetObject("header8.Image"), Image)
+        header8.Location = New Point(160, 290)
+        header8.Name = "header8"
+        header8.Size = New Size(80, 80)
+        header8.SizeMode = PictureBoxSizeMode.Zoom
+        header8.TabIndex = 4
+        header8.TabStop = False
+        ' 
+        ' header7
+        ' 
+        header7.BackgroundImageLayout = ImageLayout.None
+        header7.Image = CType(resources.GetObject("header7.Image"), Image)
+        header7.Location = New Point(30, 290)
+        header7.Name = "header7"
+        header7.Size = New Size(80, 80)
+        header7.SizeMode = PictureBoxSizeMode.Zoom
+        header7.TabIndex = 3
+        header7.TabStop = False
+        ' 
+        ' header3
+        ' 
+        header3.BackgroundImageLayout = ImageLayout.None
+        header3.Image = CType(resources.GetObject("header3.Image"), Image)
+        header3.Location = New Point(290, 30)
+        header3.Name = "header3"
+        header3.Size = New Size(80, 80)
+        header3.SizeMode = PictureBoxSizeMode.Zoom
+        header3.TabIndex = 2
+        header3.TabStop = False
+        ' 
+        ' header2
+        ' 
+        header2.BackgroundImageLayout = ImageLayout.None
+        header2.Image = CType(resources.GetObject("header2.Image"), Image)
+        header2.Location = New Point(160, 30)
+        header2.Name = "header2"
+        header2.Size = New Size(80, 80)
+        header2.SizeMode = PictureBoxSizeMode.Zoom
+        header2.TabIndex = 1
+        header2.TabStop = False
+        ' 
+        ' header1
+        ' 
+        header1.BackgroundImageLayout = ImageLayout.None
+        header1.Image = CType(resources.GetObject("header1.Image"), Image)
+        header1.Location = New Point(30, 30)
+        header1.Name = "header1"
+        header1.Size = New Size(80, 80)
+        header1.SizeMode = PictureBoxSizeMode.Zoom
+        header1.TabIndex = 0
+        header1.TabStop = False
+        ' 
+        ' mainheader
+        ' 
+        mainheader.BackColor = Color.Transparent
+        mainheader.BackgroundImageLayout = ImageLayout.Zoom
+        mainheader.Location = New Point(10, 10)
+        mainheader.Name = "mainheader"
+        mainheader.Size = New Size(96, 96)
+        mainheader.SizeMode = PictureBoxSizeMode.Zoom
+        mainheader.TabIndex = 1
+        mainheader.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("方正兰亭圆_GBK_中", 24.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(112, 10)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(260, 47)
+        Label1.TabIndex = 2
+        Label1.Text = "玩家名六个字"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("方正兰亭圆_GBK_中", 12.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(134))
+        Button1.Location = New Point(120, 66)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(150, 40)
+        Button1.TabIndex = 3
+        Button1.Text = "更改昵称"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("方正兰亭圆_GBK_中", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(134))
+        Label2.ForeColor = Color.Transparent
+        Label2.Location = New Point(12, 185)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(182, 32)
+        Label2.TabIndex = 4
+        Label2.Text = "选择一个头像"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(34, 112)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(160, 65)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 5
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.Transparent
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(212, 112)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(168, 65)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 6
+        PictureBox2.TabStop = False
+        ' 
+        ' ContextMenuStrip1
+        ' 
+        ContextMenuStrip1.ImageScalingSize = New Size(20, 20)
+        ContextMenuStrip1.Items.AddRange(New ToolStripItem() {ToolStripMenuItem1})
+        ContextMenuStrip1.Name = "ContextMenuStrip1"
+        ContextMenuStrip1.Size = New Size(244, 56)
+        ' 
+        ' ToolStripMenuItem1
+        ' 
+        ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        ToolStripMenuItem1.Size = New Size(243, 24)
+        ToolStripMenuItem1.Text = "重置此Rainmeter小组件"
+        ' 
+        ' Settings_Window
+        ' 
+        AutoScaleDimensions = New SizeF(9.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Center
+        ClientSize = New Size(420, 630)
+        ContextMenuStrip = ContextMenuStrip1
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label2)
+        Controls.Add(Button1)
+        Controls.Add(Panel1)
+        Controls.Add(Label1)
+        Controls.Add(mainheader)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        KeyPreview = True
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Settings_Window"
+        ShowIcon = False
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "鸡蛋鸭蛋荷包蛋"
+        Panel1.ResumeLayout(False)
+        CType(header6, ComponentModel.ISupportInitialize).EndInit()
+        CType(header5, ComponentModel.ISupportInitialize).EndInit()
+        CType(header4, ComponentModel.ISupportInitialize).EndInit()
+        CType(customheader, ComponentModel.ISupportInitialize).EndInit()
+        CType(header8, ComponentModel.ISupportInitialize).EndInit()
+        CType(header7, ComponentModel.ISupportInitialize).EndInit()
+        CType(header3, ComponentModel.ISupportInitialize).EndInit()
+        CType(header2, ComponentModel.ISupportInitialize).EndInit()
+        CType(header1, ComponentModel.ISupportInitialize).EndInit()
+        CType(mainheader, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ContextMenuStrip1.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents header1 As PictureBox
+    Friend WithEvents customheader As PictureBox
+    Friend WithEvents header8 As PictureBox
+    Friend WithEvents header7 As PictureBox
+    Friend WithEvents header3 As PictureBox
+    Friend WithEvents header2 As PictureBox
+    Friend WithEvents header6 As PictureBox
+    Friend WithEvents header5 As PictureBox
+    Friend WithEvents header4 As PictureBox
+    Friend WithEvents selectcustomheader As OpenFileDialog
+    Friend WithEvents mainheader As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+
+End Class
---
+++ Settings_Window.vb
@@ -0,0 +1,225 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Settings_Window
+
+    ''' <summary>
+    ''' EggyUI是由BSOD-MEMZ及其团队开发的Windows美化包，可以在Windows系统上实现类似于游戏《蛋仔派对》的界面风格。
+    ''' 这是Eggy UI 3.0 Rainmeter桌面小组件的设置程序，使用VB.NET语言编写（因为之前BSOD-MEMZ用易语言写的设置程序Bug很多，而导致用户体验不好）。
+    ''' 这次的这个设置程序加入了更多预设头像，还有修改昵称的功能
+    ''' 作者：SYSTEM-LIGHT
+    ''' 作者B站主页：https://space.bilibili.com/1591761987
+    ''' 作者GitHub主页： https://github.com/SYSTEM-LIGHT
+    ''' 蛋仔派对官网：https://party.163.com
+    ''' 欢迎来到Eggy UI官方QQ群：882583677
+    ''' 有一些代码（包括注释）是用AI写的（当然我也对这些代码修改了一下）
+    ''' </summary>
+
+#Region "头像更换功能"
+
+    ''' <summary>
+    ''' 这部分代码是用来更换Eggy UI 3.0 Rainmeter桌面小组件头像的功能。
+    ''' 头像可以是预设的，也可以是用户自定义的。
+    ''' 预设头像存储在当前目录下，文件名为header1.png, header2.png等。
+    ''' 用户自定义头像通过文件选择对话框选择，存储在指定的currentheaderfile中。
+    ''' </summary>
+
+    Private currentheaderfile As String = "header.png" '指定当前头像文件
+    Sub loadheader() '加载头像方法
+        With mainheader
+            If File.Exists(currentheaderfile) Then .Image = Image.FromFile(currentheaderfile) _
+            Else .Image = Nothing
+        End With
+    End Sub
+
+    Sub changeheader(changeheaderFile As String) '更换头像方法
+        If mainheader.Image IsNot Nothing Then
+            mainheader.Image.Dispose() '释放当前头像资源
+            mainheader.Image = Nothing '清空当前头像
+        End If
+        File.Copy(changeheaderFile, currentheaderfile, True) '复制新头像文件到当前头像文件
+        loadheader()
+    End Sub
+
+    Sub customheaderchange() '更换自定义头像方法
+        '更换自定义头像前询问用户
+        Dim result As DialogResult = MessageBox.Show(("自定义头像目前为实验性功能。" & vbCrLf & "头像图片要求：分辨率128x128，PNG格式，带Alpha通道，圆形边框（边框的周围一定要全是透明像素）" & vbCrLf & "你确定要更换自定义头像吗？"), "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+        If result = DialogResult.Yes Then
+            With selectcustomheader
+                '设置对话框标题和过滤器
+                .Title = "选择自定义头像"
+                .Filter = "受支持的图片文件|*.png;"
+                .FilterIndex = 1 '默认选择PNG文件
+                .RestoreDirectory = True '还原上次选择的目录
+                Try
+                    If .ShowDialog() = DialogResult.OK Then
+                        ' 用户选择了文件
+                        Dim selectedFile As String = .FileName
+                        ' 复制或处理选中的文件
+                        changeheader(selectedFile)
+                        MessageBox.Show("自定义头像更换成功，请刷新Rainmeter显示。", "提示",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    End If
+                Catch ex As Exception
+                    loadheader()
+                    MessageBox.Show("自定义头像更换失败，原因是发生以下异常：" & vbCrLf & ex.Message & vbCrLf & "如果你要反馈此问题，请勿直接反馈给Eggy UI项目组，请将此错误消息截图并发送给@冷落的小情绪。", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                End Try
+            End With
+        End If
+    End Sub
+
+    Sub presetheaderchange(sender As Object) '更换预设头像方法
+        Dim clickedHeader As PictureBox = CType(sender, PictureBox) '获取点击的预设头像控件
+        Dim headerfile As String = clickedHeader.Name & ".png" '预设头像文件名
+        If File.Exists(headerfile) Then '检查头像文件是否存在
+            '替换头像
+            changeheader(headerfile)
+            MessageBox.Show("头像更换成功，请刷新Rainmeter显示。", "提示",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Else
+            '显示错误消息
+            MessageBox.Show("指定的预设头像文件不存在: " & headerfile, "错误",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub Header_Click(sender As Object, e As EventArgs) Handles header1.Click,
+            header2.Click, header3.Click, header4.Click, header5.Click, header6.Click,
+            header7.Click, header8.Click
+        '处理预设头像点击事件
+        presetheaderchange(sender)
+    End Sub
+
+    Private Sub customheader_Click(sender As Object, e As EventArgs) Handles customheader.Click
+        '处理自定义头像点击事件
+        customheaderchange()
+    End Sub
+
+#End Region
+
+#Region "用户昵称更改功能"
+
+    ''' <summary>
+    ''' 这部分代码是用来更改Eggy UI 3.0 Rainmeter桌面小组件的当前用户昵称的功能。
+    ''' 昵称存储在home.ini文件的第36行（索引为35）中，格式为Text=当前用户昵称。
+    ''' loadName方法用于加载当前用户昵称并显示在Label1上。
+    ''' changeName方法用于修改当前用户昵称，用户可以通过输入框输入新的用户昵称，点击按钮后更新显示的用户昵称和home.ini文件。
+    ''' </summary>
+
+    Sub loadName() '加载昵称方法
+        If File.Exists("home.ini") Then
+            Dim lines() As String = File.ReadAllLines("home.ini")
+            Dim lineIndex As Integer = 35 '第36行，索引从0开始
+            Dim Name As String = ""
+            If lines.Length > lineIndex Then
+                Dim line As String = lines(lineIndex)
+                If line.StartsWith("Text=") Then
+                    Name = line.Substring("Text=".Length).Trim()
+                Else
+                    Name = line.Trim()
+                End If
+            End If
+            Label1.Text = Name '设置Label1的文本为读取到的用户昵称
+        End If
+    End Sub
+
+    Sub changeName() '修改昵称方法
+        Dim newName As String = InputBox("请输入新的昵称：", "更改昵称")
+        Try
+            If Not String.IsNullOrWhiteSpace(newName) Then
+                '如果用户输入了新的用户昵称，则更新home.ini文件和显示的用户昵称
+                Dim lines() As String = File.ReadAllLines("home.ini") '读取home.ini文件的所有行
+                Dim lineIndex As Integer = 35 '第36行，索引从0开始
+                If lines.Length > lineIndex Then
+                    lines(lineIndex) = "Text=" & newName
+                    File.WriteAllLines("home.ini", lines, System.Text.Encoding.Unicode)
+                End If
+                Label1.Text = newName
+                MessageBox.Show("昵称更改成功，请刷新Rainmeter显示。", "提示", MessageBoxButtons.OK,
+                                MessageBoxIcon.Information)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("昵称更改失败，原因是发生以下异常：" & vbCrLf & ex.Message & vbCrLf & "如果你要反馈此问题，请勿直接反馈给Eggy UI项目组，请将此错误消息截图并发送给@冷落的小情绪。", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub changeNameButton_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        changeName()
+    End Sub
+
+#End Region
+
+#Region "重置Rainmeter组件功能"
+
+    Sub ResetRainmeter()
+        Dim fsw As New StreamWriter("home.ini", False, System.Text.Encoding.Unicode)
+        fsw.Write("[Rainmeter]" + vbCrLf +
+                  "Update=1000" + vbCrLf +
+                  "Logging=0" + vbCrLf +
+                  "SkinPath=%USERPROFILE%\Documents\Rainmeter\Skins\" +
+                  vbCrLf + vbCrLf +
+                  "[MeterLaunch1Image]" + vbCrLf +
+                  "Meter=Image" + vbCrLf +
+                  "ImageName=home.png" + vbCrLf +
+                  "W=285" + vbCrLf +
+                  "H=96" + vbCrLf +
+                  ";在W,H中设置图像的长和宽" + vbCrLf +
+                  "X=32" + vbCrLf +
+                  "Y=32" + vbCrLf +
+                  "LeftMouseUpAction=[""%USERPROFILE%\Documents\Rainmeter\Skins\EggyUI\Home\EggyUIDesktopWidgetsSettings.exe""]" +
+                  vbCrLf + vbCrLf +
+                  "[MeterLaunch1tx]" + vbCrLf +
+                  "Meter=Image" + vbCrLf +
+                  "ImageName=header.png" + vbCrLf +
+                  "W=75" + vbCrLf +
+                  "H=75" + vbCrLf +
+                  ";在W,H中设置图像的长和宽" + vbCrLf +
+                  "X=43" + vbCrLf +
+                  "Y=43" + vbCrLf +
+                  "LeftMouseUpAction=[""%USERPROFILE%\Documents\Rainmeter\Skins\EggyUI\Home\EggyUIDesktopWidgetsSettings.exe""]" +
+                  vbCrLf + vbCrLf +
+                  "[MeterLaunch1Text]" + vbCrLf +
+                  "Meter=String" + vbCrLf +
+                  "X=133" + vbCrLf +
+                  "Y=50" + vbCrLf +
+                  "FontFace=Impact" + vbCrLf +
+                  "FontSize=14" + vbCrLf +
+                  "FontColor=255,255,255,255" + vbCrLf +
+                  ";StringStyle=Bold" + vbCrLf +
+                  "SolidColor=0,0,0,1" + vbCrLf +
+                  "AntiAlias=1" + vbCrLf +
+                  "Text=Eggy" + vbCrLf +
+                  ";将Text=后面的内容修改为您的用户名" + vbCrLf +
+                  "LeftMouseUpAction=[""%USERPROFILE%\Documents\Rainmeter\Skins\EggyUI\Home\EggyUIDesktopWidgetsSettings.exe""]" + vbCrLf + vbCrLf)
+        fsw.Close()
+        If mainheader.Image IsNot Nothing Then
+            mainheader.Image.Dispose()
+            mainheader.Image = Nothing
+        End If
+        changeheader("header1.png")
+        loadName()
+        MessageBox.Show("重置此Rainmeter小组件成功！", "提示",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        ResetRainmeter()
+    End Sub
+
+#End Region
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '处理窗口加载事件
+        loadheader() '加载当前头像
+        loadName() '加载当前用户昵称
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If (e.KeyCode = Keys.Escape) Then End '按下ESC键退出程序
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        MessageBox.Show("此功能暂未开放。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
